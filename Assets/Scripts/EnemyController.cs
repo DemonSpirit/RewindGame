471cab1929133d7f00419b98bb28fb67e7b79b76
Wichaya Karnchanapee <wkarnchanapee@gmail.com>
2017-10-21 18:42:27 +0100

    adding scene - AI testing stage

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public string state = "idle";
+    float maxDist = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case "idle":
+                LookForPlayer();
+                break;
+        }
+    }
+
+    void LookForPlayer()
+    {
+        RaycastHit hit;
+        for (float i = -1f; i < 1 ; i += 0.25f)
+        {
+            for (int ii = 0; ii < length; ii++)
+            {
+
+            }
+            Physics.Raycast(transform.position, new Vector3(i,0f,ii), out hit, maxDist);
+
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float frequency = 20.0f; // speed of sine movement
+    public float magnitude = 0.5f; // size of sine movement
+    Vector3 axis;
+    Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+        
+        axis = transform.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //pos += transform.up * Time.deltaTime * moveSpeed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+	}
+}
