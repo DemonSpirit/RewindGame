08f207e4148567ddb8cc8662c6b6dd0ccfc642a5
Billy-Desktop <wkarnchanapee@gmail.com>
2017-10-21 22:26:25 +0100

    Added player detection to AI.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,34 +6,69 @@ public class EnemyController : MonoBehaviour {
 
     public string state = "idle";
     float maxDist = 10f;
+    float turnSpeed = 0.1f;
+    public Transform target;
+    public GameControl gameControl;
 
+    Vector3 startPos;
+    Quaternion startRot;
+    public float shootDist = 10f;
 	// Use this for initialization
 	void Start () {
-		
+        startPos = transform.position;
+        startRot = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        switch (state)
+        switch (gameControl.gameState)
         {
-            case "idle":
-                LookForPlayer();
+            case "start":
+                transform.position = startPos;
+                transform.rotation = startRot;
+
+                break;
+            case "live":
+                switch (state)
+                {
+                    case "idle":
+                        //LookForPlayer();
+                        break;
+                    case "alert":
+                        LookAtTarget();
+                        Vector3 dir = target.position - transform.position;
+                        Debug.DrawRay(transform.position, dir, Color.green);
+
+                        RaycastHit hit;
+                        if (Physics.Raycast(transform.position,dir,out hit, shootDist))
+                        {
+                            Debug.Log(hit.transform.name);
+                            
+                            // I do this because hit.transform will give me the cone collider not the parent collider.
+                            if (hit.transform == target)
+                            {
+                                //Target is in range
+                                Debug.Log("Target is in range");
+                                
+                            }
+
+                        }
+                        break;
+                }
+                break;
+            case "end":
+                state = "idle";
+                target = null;
+                
                 break;
         }
+        
     }
 
-    void LookForPlayer()
+    void LookAtTarget()
     {
-        RaycastHit hit;
-        for (float i = -1f; i < 1 ; i += 0.25f)
-        {
-            for (int ii = 0; ii < length; ii++)
-            {
-
-            }
-            Physics.Raycast(transform.position, new Vector3(i,0f,ii), out hit, maxDist);
-
-        }
+        Quaternion toRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.time * turnSpeed);
         
     }
 }
---
+++ Assets/SightCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightCollider : MonoBehaviour {
+
+    public EnemyController owner;
+    private void Awake()
+    {
+        owner = GetComponentInParent<EnemyController>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (owner.state == "idle")
+        {
+            Debug.Log(other.name + " entered trigger!");
+            owner.target = other.transform;
+            owner.state = "alert";
+        }
+    }
+}
