f700c9140d6b38697268dd82ccbbb087c1c8c1ed
Billy-Desktop <wkarnchanapee@gmail.com>
2017-12-23 08:39:42 +0000

    added colour changing script

---
+++ Assets/Scripts/GetColourForMatShade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetColourForMatShade : MonoBehaviour {
+    PlayerController playCtrl;
+    [SerializeField]Material mat;
+	// Use this for initialization
+	void Update () {
+        if (GameControl.main.gameState == "live")
+        {
+            playCtrl = transform.GetComponentInParent<PlayerController>();
+            mat = GetComponent<Renderer>().material;
+            mat.color = Color.blue;
+        }
+        
+        
+	}
+	
+}
