faab17b99ec9b377bbbd1ff20fda86341f923867
Wichaya Karnchanapee <wkarnchanapee@gmail.com>
2018-01-14 18:27:35 +0000

    attempt1 at dialogue sequencing locking narrative (it didnt work but i left the code here incase for future)

--- Assets/Scripts/Solo/DialogueTrigger.cs
+++ Assets/Scripts/Solo/DialogueTrigger.cs
@@ -7,19 +7,35 @@ public class DialogueTrigger : MonoBehaviour
     [SerializeField] Text text;
     [SerializeField] string[] str;
     [SerializeField] public bool triggered = false;
+    [SerializeField] int runAtKeyStage = 0;
     float alpha = 1f;
     [SerializeField] float holdTime = 1.5f;
     [SerializeField]public bool resetable = false;
+    [SerializeField] float countdown = 0;
+    [SerializeField] float countdownTime;
+    [SerializeField] float toNarrativeStage;
 
     private void Start()
     {
         text = SoloGameController.main.textBox;
+        
+    }
+
+    private void Update()
+    {
+        if (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
+        } else if (countdown < 0)
+        {
+            SoloGameController.main.narrativeStage = toNarrativeStage;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
 
-        if (other.tag == "Player" && triggered == false)
+        if (other.tag == "Player" && triggered == false && SoloGameController.main.keys == runAtKeyStage)
         {
             //Text text = SoloGameController.main.textBox;
             //text.text = str[0];
@@ -33,6 +49,8 @@ public class DialogueTrigger : MonoBehaviour
             animCtrl.holdTime = holdTime;
             triggered = true;
             animCtrl.curTrigger = this;
+            countdownTime = holdTime * str.Length;
+            countdown = countdownTime;
 
         }
     }
