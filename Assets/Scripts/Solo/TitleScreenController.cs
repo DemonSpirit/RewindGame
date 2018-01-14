b8b162df20b246c637068f821312f8300c5af179
Wichaya Karnchanapee <wkarnchanapee@gmail.com>
2018-01-14 19:40:21 +0000

    level select title screen

--- Assets/Scripts/Solo/TitleScreenController.cs
+++ Assets/Scripts/Solo/TitleScreenController.cs
@@ -11,11 +11,21 @@ public class TitleScreenController : MonoBehaviour
     }
     public void GoToVersus()
     {
-        SceneManager.LoadScene("doruk edt 1", LoadSceneMode.Single);
+        SceneManager.LoadScene("level select", LoadSceneMode.Single);
     }
     public void ExitGame()
     {
         Application.Quit();
         print("Sorry, you can't leave!!!");
+    }
+    public void GoToStadium()
+    {
+        
+        SceneManager.LoadScene("Arena", LoadSceneMode.Single);
+    }
+    public void GoToDistrict()
+    {
+
+        SceneManager.LoadScene("doruk edt 1", LoadSceneMode.Single);
     }
 }
